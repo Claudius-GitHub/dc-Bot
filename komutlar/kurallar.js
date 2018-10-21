a590186262295d27d27190d31f52dddfbbb2786b
Claudius <ik@onuraksoy.com.tr>
2018-10-21 05:13:13 +0300

    Update
    update

--- komutlar/kurallar.js
+++ komutlar/kurallar.js
@@ -2,59 +2,78 @@ const Discord = require('discord.js');
 
 
 exports.run = function(client, message) {
-	
+const oyunTakimi = [
+	"334399841931231232", //claudius
+	"424897059007758337", //aiakos
+	"435846972163883008", //asklepios
+	"479732372938555413", //nugget
+	"308698577734205450", //snape
+	"334404631402905601", //pro
+	"373034927299756032" //geralt	
+];
+const forumTakimi = [
+	"356015009459077120", //chriger
+	"441307434708107275", //jai
+	"502119966879907840", //luke
+	"493131520765198356", //luke2
+	"493752808886239232", //nemesis
+	"495206584897634305", //oblomov	
+	"496057652015792139", //revan
+];
+var op1 = oyunTakimi[0];
+var op2 = oyunTakimi[1];
+var op3 = oyunTakimi[2];
+var op4 = oyunTakimi[3];
+var op5 = oyunTakimi[4];
+var op6 = oyunTakimi[5];
+var op7 = oyunTakimi[6];
+var mod1 = forumTakimi[0];
+var mod2 = forumTakimi[1];
+var mod3 = forumTakimi[2];
+var mod4 = forumTakimi[3];
+var mod5 = forumTakimi[4];
+var mod6 = forumTakimi[5];
+var mod7 = forumTakimi[6];
+var mesaj = args.slice(0).join(' ');
 
-	var guildID = "325962675035045889";
-	var channelID = "502893448186298370";
-	
-	
-	
-if (message.author.id === "334399841931231232") {	
-message.channel.send("#Support kanalını kontrol et...");
-	client.guilds.get(guildID).channels.get(channelID).send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "İhlaller ve Uzaklaştırma süreleri.",
-    description: "",
-    fields: [{
-        name: "x ihlal",
-        value: "x. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \n"
-      },
-			 {
-        name: "x ihlal",
-        value: "x. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \n"
-      },
-			 {
-        name: "x ihlal",
-        value: "x. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \n"
-      },
-			 {
-        name: "x ihlal",
-        value: "x. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \nx. ihlal - x süre \n"
-      },
-     
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Claudius"
-    }
-  }
-});
-}else if (message.author.id === "308698577734205450") {	//snape
-message.reply("Sana kural yok...");
+	if (message.author.id === op1 || message.author.id === op2 || message.author.id === op3 || message.author.id === op4 || message.author.id === op5 || message.author.id === op6|| message.author.id === op7) {
+		if (mesaj === "multi"){			
+	const embed = new Discord.RichEmbed()
+      .setColor(0xc0392b)
+      .addField("Multi İhlaller","Multi kurallar burada yer alacaktır.")
+      message.channel.send({embed});
+		}else if (mesaj === "hesappaylaşımı"){			
+	const embed = new Discord.RichEmbed()
+      .setColor(0xc0392b)
+      .addField("HESAP PAYLAŞIMI","HESAP PAYLAŞIMI kuralları burada yer alacaktır.")
+      message.channel.send({embed});
+		}else if (mesaj === "bot"){			
+	const embed = new Discord.RichEmbed()
+      .setColor(0xc0392b)
+      .addField("BUG & SCRIPT KULLANIMI ","Scprit kuralları burada yer alacaktır.")
+      message.channel.send({embed});
+		}else if (mesaj === "help"){			
+	const embed = new Discord.RichEmbed()
+	.setColor(0xc0392b)
+	.addField("multi kuralları ","!ihlal multi yazak ulaşabilirsiniz..")
+	.addField("hesap paylaşımı kuralları ","!ihlal hesappaylaşımı yazak ulaşabilirsiniz..")
+	.addField("Bug Script kullanımı ","!ihlal bot yazak ulaşabilirsiniz..")
+      message.channel.send({embed});
+		}else {message.reply("!ihlal help komutu ile ayrıntılı bilgi alabilirsiniz.")}
+		
+	}else if (message.author.id === mod1 || message.author.id === mod2 || message.author.id === mod3 || message.author.id === mod4 || message.author.id === mod5 || message.author.id === mod6 || message.author.id === mod7) {
+		
+		message.channel.send("hmm. Demek forum takımındasın. Bana moderatasyon hakkında hiç bir şey öğretilmedi. @claudius ile iletişime geçebilirsin.");
+	} else {
+		message.channel.send("Hangi taraftasın sen ? Kaçak girdin sanırım.");
+	}
 
-}else if (message.author.id === "334723793266409475") {	
-message.reply("paten serbest. ihlal yok...");
-
-}//elseif
 	
 	
-	else 
-		  message.reply('Seni tanımıyorum Bro.')
+	
+	
+ 
+	
 	
 }
 
